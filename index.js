9ebd96d1cc1af35276fb7e57e7485a2923bcdeb9
Anton Zakharov <anton.zakharov@wdg.com.ua>
2018-01-29 14:34:51 +0200

    add header promise add header styles

--- index.js
+++ index.js
@@ -16,18 +16,32 @@ connection.connect();
 app.set('view engine' , 'ejs');
 app.use('/public', express.static('public'));
 
-app.get('/portfolio',function (req, res) {
-    connection.query('SELECT * FROM progect ORDER by type; SELECT * FROM menu', [1,2], function (err, row) {
+var headerPromise = new Promise(function (resolve, reject) {
+
+    connection.query('SELECT * FROM menu', function (err, row) {
         if (err) throw err;
-        res.render('catalog', {menu: row[1] , post: row[0]});
+        app.render('blocks/header', { menu:row}, function (opts, header) {
+            resolve(header);
+        });
+    });
+
+});
+
+app.get('/portfolio',function (req, res) {
+    connection.query('SELECT * FROM progect ORDER by type', function (err, row) {
+        if (err) throw err;
+        headerPromise.then(function (header) {
+            res.render('catalog', { post: row, header: header});
+        })
     });
 
 });
 
 app.get('/portfolio/:ids',function (req, res) {
     var targetId = req.params.ids;
-    connection.query('SELECT * FROM progect WHERE id= ?; SELECT * FROM menu', [targetId, 1,2], function (err, row) {
+    connection.query('SELECT * FROM progect WHERE id= ?; SELECT * FROM menu', [targetId], function (err, row) {
         if (err) throw err;
+        console.log(row[0]);
         res.render('product-page', {
             post: row[0][0],
             menu: row[1]
@@ -35,7 +49,7 @@ app.get('/portfolio/:ids',function (req, res) {
     });
 });
 
-app.listen(8000);
+app.listen(3000);
 
 
 
