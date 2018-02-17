5163b5cd121589fe9d74a2ddd16a7608cfc68b92
West <toxawest@gmail.com>
2018-02-17 11:56:54 +0200

    added routes ,masonry

--- client/src/blocks/posts.js
+++ client/src/blocks/posts.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import Masonry from 'react-masonry-component';
+
+const masonryOptions = {
+    transitionDuration: 0,
+};
 
 class Posts extends Component {
     state = {post: []};
@@ -10,23 +15,31 @@ class Posts extends Component {
     }
 
     render() {
-        return (
-            <div className="post">
-                {this.state.post.map(post =>
-                    <div key={post.id} className="post-single">
-                        <h2><a href={'/post/' + post.id} title={post.title}>{post.title}</a></h2>
-                        <div className="post-single__image">
-                            <img src={ '/public/content/' + post.images} alt={post.title}/>
-                        </div>
-                        <div className="post-single__description">
-                            <span>{post.description}</span>
-                        </div>
-                        <div className="post-single__type">
-                            <span>{post.type}</span>
-                        </div>
-                    </div>
-                )}
+        document.title = "Posts";
+        const posts = this.state.post.map(post =>
+            <div key={post.id} className="post-single">
+                <h2><a href={'/post/' + post.id} title={post.title}>{post.title}</a></h2>
+                <div className="post-single__image">
+                    <img src={'/public/content/' + post.images} alt={post.title}/>
+                </div>
+                <div className="post-single__description">
+                    <span>{post.description}</span>
+                </div>
+                <div className="post-single__type">
+                    <span>{post.type}</span>
+                </div>
             </div>
+        );
+        return (
+            <Masonry
+                className={'post'}
+                elementType={'div'}
+                options={masonryOptions}
+                disableImagesLoaded={false}
+                updateOnEachImageLoad={true}
+            >
+                {posts}
+            </Masonry>
         );
     }
 }
