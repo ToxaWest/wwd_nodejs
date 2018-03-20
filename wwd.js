a7ce3aab4a5a3b0b487934585437b42b8aed50b9
Anton Zakharov <anton.zakharov@wdg.com.ua>
2018-03-20 14:32:34 +0200

    add ssl

--- wwd.js
+++ wwd.js
@@ -1,8 +1,16 @@
 const   express = require('express'),
         bodyParser = require('body-parser'),
         path = require('path'),
+        fs = require('fs'),
+        http = require('http'),
+        https = require('https'),
+        privateKey  = fs.readFileSync('./sslcert/wwd.com.test.key', 'utf8'),
+        certificate = fs.readFileSync('./sslcert/wwd.com.test.cert', 'utf8'),
+        credentials = {key: privateKey, cert: certificate},
         app = express(),
-        port = 3000;
+        port = 3007,
+        SSLport = 3000;
+
 
 const posts  = require('./routes/posts');
 const services  = require('./routes/services');
@@ -17,8 +25,11 @@ app.use('/services', services);
 
 module.exports = app;
 
-app.listen(port);
-console.log('Nodejs start at http://localhost:' + port);
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(port);
+httpsServer.listen(SSLport);
 
 
 
